0ee053a7b3d56752ae7f81242f91669437e0c024
Wojtek Wojtunik <desqa16@gmail.com>
2020-01-01 23:09:46 +0100

    Updates

---
+++ precache-manifest.ae894ea8c1726d7325da0a5b634a1cca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3f8c32cf6a0aac0ad8f810e5855ff30",
+    "url": "/Pogodeska/index.html"
+  },
+  {
+    "revision": "45c2f5a7b9a977d5a6d8",
+    "url": "/Pogodeska/static/css/main.04adc203.chunk.css"
+  },
+  {
+    "revision": "3b621a6fcd235165de68",
+    "url": "/Pogodeska/static/js/2.c24b131b.chunk.js"
+  },
+  {
+    "revision": "45c2f5a7b9a977d5a6d8",
+    "url": "/Pogodeska/static/js/main.02bcef41.chunk.js"
+  },
+  {
+    "revision": "d76d8c143ce3ae3689a2",
+    "url": "/Pogodeska/static/js/runtime~main.c22b2ce3.js"
+  }
+]);
